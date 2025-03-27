7c8a09c652feef218340c7a2a03e333a806ecd99
Corey Donahue <cdartswebdev@gmail.com>
2025-03-27 15:25:09 -0600

    Use ! on the environment variable is you 100% know it will be  there at run time
    const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET! as string;

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -5,7 +5,7 @@ import { prisma } from "@/lib/prisma"; // <-- import Prisma client
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
-const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET as string;
+const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET! as string;
 
 export async function POST(req: NextRequest) {
   const body = await req.text();
