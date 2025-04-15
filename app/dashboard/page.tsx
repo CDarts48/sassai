a573c78bc0fdf97b9ca01d7288c4af4be9388226
Corey Donahue <cdartswebdev@gmail.com>
2025-04-15 14:44:00 -0600

    added a dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+
+
+function Dashboard() {
+  return (
+    <div className = "px-4 py-8 sm:py-12 lg:py-16 max-w-7xl mx-auto">
+      <h1>Dashboard</h1>
+      <p>Welcome to the dashboard!</p>
+    </div>
+  );
+}
+
+export default Dashboard;
